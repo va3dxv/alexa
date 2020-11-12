854e6360b0916f6589ae503036428fbac46f09fa
Brian Graves <va3dxv@gmail.com>
2020-11-12 01:05:07 -0500

    .

--- lambda/local_debug.py
+++ lambda/local_debug.py
@@ -2,10 +2,10 @@ from ask_sdk_local_debug.local_debugger_invoker import LocalDebuggerInvoker
 
 if __name__ == "__main__":
     LocalDebuggerInvoker([
-        '--accessToken', '<ACCESS_TOKEN>',
-        '--skillId', 'amzn1.ask.skill.b5c3a293-cf6d-43aa-b94d-ffb55c6ddf7a',
+        '--accessToken', '2:TZsLh3B9ti7vpPQ5xbqNHwiJCJt90_FixzgzB8mZ1Trau0dRREHjBunPth0cXQJK:3XEXEtAXBA-kh_b1xG8Seg==',
+        '--skillId', 'amzn1.ask.skill.cb2476b1-d8fd-494b-b659-cdcabcd7ce63',
         '--skillHandler', 'lambda_handler',
         '--skillFilePath', 'lambda_function.py',
-        '--region', 'NA(North America)'
+        '--region', 'arn:aws:lambda:us-east-1:598628761688:function:cb2476b1-d8fd-494b-b659-cdcabcd7ce63:Release_0'
         ]
     ).invoke()
